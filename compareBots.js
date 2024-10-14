32c82b252f4ade198024806671687204b3f5c7ff
Mauro Roberto da Cruz Cunha <41219319+mrccunha@users.noreply.github.com>
2024-10-14 19:04:35 -0300

    Update compareBots.js

--- compareBots.js
+++ compareBots.js
@@ -7,88 +7,64 @@ const outputPath = 'bots/bot_comparison_prod_vs_homolog.json';
 
 const dir = path.dirname(outputPath);
 if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
+  fs.mkdirSync(dir, { recursive: true })
 }
-
 // Função para carregar e decodificar o bot
 function loadAndDecodeBot(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, content) => {
-            if (err) {
-                return reject(`Erro ao carregar o arquivo: ${err.message}`);
-            }
+  const botData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-            const base64PartStart = content.lastIndexOf('["eyJuYW1l"'); // Identifica onde começa a parte base64
+  // Decodificar os diálogos
+  const decodedDialogs = botData.dialogs.map(encodedDialog => {
+    const decoded = base64.decode(encodedDialog);
+    return JSON.parse(decoded);  // Converter a string JSON decodificada em objeto JavaScript
+  });
 
-            const initialPart = content.substring(0, base64PartStart); // Parte inicial
-            const base64Part = content.substring(base64PartStart); // Parte em base64
-
-            // Decodifica a parte em base64
-            let decodedDialogs = {};
-            if (base64Part) {
-                const cleanedBase64 = base64Part.replace(/[\r\n]+/g, '');
-                const decodedString = atob(cleanedBase64);
-                decodedDialogs = JSON.parse(decodedString);
-            }
-
-            // Retorna o JSON combinado
-            resolve({
-                initial: JSON.parse(initialPart), // Parte inicial como objeto JSON
-                dialogs: decodedDialogs // Parte decodificada
-            });
-        });
-    });
+  return decodedDialogs;
 }
 
 // Função para normalizar os dados e remover campos irrelevantes
 function normalizeBotData(decodedDialogs) {
-    if (!Array.isArray(decodedDialogs)) {
-        return decodedDialogs; // Retorna como está se não for um array
-    }
+  decodedDialogs.forEach(dialog => {
+    dialog.config.cells.forEach(cell => {
+      delete cell.id;
+      delete cell.position;
+      delete cell.size;
 
-    decodedDialogs.forEach(dialog => {
-        if (dialog.config && dialog.config.cells) {
-            dialog.config.cells.forEach(cell => {
-                delete cell.id;
-                delete cell.position;
-                delete cell.size;
+      // Remover atributos visuais que não impactam o comportamento
+      if (cell.attrs) {
+        delete cell.attrs
+      }
 
-                // Remover atributos visuais que não impactam o comportamento
-                if (cell.attrs) {
-                    delete cell.attrs;
-                }
+      if (cell.events) {
+        delete cell.events;
+      }
 
-                if (cell.events) {
-                    delete cell.events;
-                }
-            });
-        }
     });
-    return decodedDialogs;
+  });
+  return decodedDialogs;
 }
 
 // Carregar e comparar os arquivos dos bots de produção e homologação
-Promise.all([
-    loadAndDecodeBot('export_builder_prod.json'),
-    loadAndDecodeBot('export_builder_homolog.json')
-]).then(([botProdResult, botHomologResult]) => {
-    if (botProdResult && botHomologResult) {
-        const botProdNormalized = normalizeBotData(botProdResult.dialogs);
-        const botHomologNormalized = normalizeBotData(botHomologResult.dialogs);
+const botProdDialogs = loadAndDecodeBot('export_builder_prod.json');  // Arquivo do bot de produção
+const botHomologDialogs = loadAndDecodeBot('export_builder_homolog.json');  // Arquivo do bot de homologação
 
-        // Comparar os diálogos decodificados e normalizados dos dois bots
-        const diff = deepDiff(botProdNormalized, botHomologNormalized);
+// Normalizar os dados dos bots
+const botProdNormalized = normalizeBotData(botProdDialogs);
+const botHomologNormalized = normalizeBotData(botHomologDialogs);
 
-        // Verificar se há diferenças
-        if (diff) {
-            fs.writeFileSync(outputPath, JSON.stringify(diff, null, 4));
-            console.log("Diferenças encontradas entre os bots de produção e homologação. Resultado salvo em bot_comparison_prod_vs_homolog.json");
-        } else {
-            console.log("Os bots de produção e homologação são idênticos após a normalização.");
-        }
-    } else {
-        console.error('Erro ao carregar um ou ambos os bots.');
-    }
-}).catch(error => {
-    console.error('Erro ao processar os bots:', error);
-});
+// Comparar os diálogos decodificados e normalizados dos dois bots
+const diff = deepDiff(botProdNormalized, botHomologNormalized);
+
+// Verificar se há diferenças
+if (diff) {
+  fs.writeFileSync(outputPath, JSON.stringify(diff, null, 4));
+  fs.writeFileSync('bot_normalized_1.json', JSON.stringify(botProdNormalized, null, 4));
+  fs.writeFileSync('bot_normalized_2.json', JSON.stringify(botHomologNormalized, null, 4));
+  //fs.writeFileSync('botDialogs_1.json', JSON.stringify(botProdDialogs, null, 4));
+  //fs.writeFileSync('botDialogs_2.json', JSON.stringify(botHomologDialogs, null, 4));
+  console.log("Diferenças encontradas entre os bots de produção e homologação. Resultado salvo em bot_comparison_prod_vs_homolog.json");
+} else {
+  //fs.writeFileSync('bot_normalized_1.json', JSON.stringify(bot1Normalized, null, 4));
+  //fs.writeFileSync('bot_normalized_2.json', JSON.stringify(bot2Normalized, null, 4));
+  console.log("Os bots de produção e homologação são idênticos após a normalização.");
+}
